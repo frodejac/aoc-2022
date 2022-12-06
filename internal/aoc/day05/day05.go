beb548e93932e97612f785463de61c08e6733c84
Frode <frode@remin.no>
2022-12-06 06:50:00 +0100

    Day 5, parts 1 & 2

--- internal/aoc/day05/day05.go
+++ internal/aoc/day05/day05.go
@@ -1,17 +1,46 @@
 package day05
 
+import (
+	"strconv"
+)
+
 type Day05 struct {
-	input []byte
+	input string
 }
 
 func Solver(input []byte) *Day05 {
-	return &Day05{input: input}
+	return &Day05{input: string(input)}
+}
+
+func checkSegmentMarker(sequence string) bool {
+	for i := 0; i < len(sequence); i++ {
+		for j := i + 1; j < len(sequence); j++ {
+			if sequence[i] == sequence[j] {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func (d *Day05) SolvePart1() string {
-	return "Day 05, part 1"
+	markerPos := 0
+	for i := 4; i < len(d.input); i++ {
+		if checkSegmentMarker(d.input[i-4 : i]) {
+			markerPos = i
+			break
+		}
+	}
+	return strconv.Itoa(markerPos)
 }
 
 func (d *Day05) SolvePart2() string {
-	return "Day 05, part 2"
+	markerPos := 0
+	for i := 14; i < len(d.input); i++ {
+		if checkSegmentMarker(d.input[i-14 : i]) {
+			markerPos = i
+			break
+		}
+	}
+	return strconv.Itoa(markerPos)
 }
