a52122c5bad96485eafdd24a4b3e01b584201836
Frode <frode@remin.no>
2022-12-06 09:08:37 +0100

    Refactor out duplicated code

--- internal/aoc/day05/day05.go
+++ internal/aoc/day05/day05.go
@@ -12,7 +12,7 @@ func Solver(input []byte) *Day05 {
 	return &Day05{input: string(input)}
 }
 
-func checkSegmentMarker(sequence string) bool {
+func isSegmentMarker(sequence string) bool {
 	for i := 0; i < len(sequence); i++ {
 		for j := i + 1; j < len(sequence); j++ {
 			if sequence[i] == sequence[j] {
@@ -23,24 +23,21 @@ func checkSegmentMarker(sequence string) bool {
 	return true
 }
 
-func (d *Day05) SolvePart1() string {
-	markerPos := 0
-	for i := 4; i < len(d.input); i++ {
-		if checkSegmentMarker(d.input[i-4 : i]) {
-			markerPos = i
-			break
+func findSegmentMarker(sequence string, markerLength int) int {
+	for i := markerLength; i < len(sequence); i++ {
+		if isSegmentMarker(sequence[i-markerLength : i]) {
+			return i
 		}
 	}
+	return -1
+}
+
+func (d *Day05) SolvePart1() string {
+	markerPos := findSegmentMarker(d.input, 4)
 	return strconv.Itoa(markerPos)
 }
 
 func (d *Day05) SolvePart2() string {
-	markerPos := 0
-	for i := 14; i < len(d.input); i++ {
-		if checkSegmentMarker(d.input[i-14 : i]) {
-			markerPos = i
-			break
-		}
-	}
+	markerPos := findSegmentMarker(d.input, 14)
 	return strconv.Itoa(markerPos)
 }
