5be20d83aa312773269dff2a52420c8431eef3c5
Frode <frode@remin.no>
2022-12-09 13:23:03 +0100

    Day08 visualization

--- internal/aoc/day08/day08.go
+++ internal/aoc/day08/day08.go
@@ -1,7 +1,12 @@
 package day08
 
 import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"math"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -165,4 +170,62 @@ func (d *Day08) SolvePart2() string {
 		r.move(move.dir, move.dst)
 	}
 	return strconv.Itoa(len(r.tail().visited))
+}
+
+func (r *rope) visualize(w, h int, palette []color.Color) *image.Paletted {
+	centerX, centerY := w/2, h/2
+	img := image.NewPaletted(image.Rect(0, 0, w, h), palette)
+	for i := len(r.knots) - 1; i >= 0; i-- {
+		k := r.knots[i]
+		knotX, knotY := centerX+k.position.X*9, centerY+k.position.Y*9
+		for x := knotX - 4; x <= knotX+4; x++ {
+			for y := knotY - 4; y <= knotY+4; y++ {
+				img.Set(x, y, palette[i+1])
+			}
+		}
+	}
+	return img
+}
+
+func (d *Day08) Visualize() {
+	var w, h int = 500, 500
+
+	var palette = []color.Color{
+		color.RGBA{0x00, 0x00, 0x00, 0xff},
+		color.RGBA{250, 250, 110, 0xff},
+		color.RGBA{207, 241, 116, 0xff},
+		color.RGBA{166, 229, 125, 0xff},
+		color.RGBA{129, 216, 134, 0xff},
+		color.RGBA{93, 202, 143, 0xff},
+		color.RGBA{60, 187, 150, 0xff},
+		color.RGBA{28, 170, 153, 0xff},
+		color.RGBA{1, 154, 151, 0xff},
+		color.RGBA{13, 137, 146, 0xff},
+		color.RGBA{33, 120, 136, 0xff},
+	}
+	var images []*image.Paletted
+
+	r := getRope(10)
+	for _, move := range d.moves[:300] {
+
+		for i := 0; i < move.dst; i++ {
+			r.head().move(move.dir)
+			img := r.visualize(w, h, palette)
+			images = append(images, img)
+			for j := 1; j < len(r.knots); j++ {
+				r.knots[j].update(r.knots[j-1].position.X, r.knots[j-1].position.Y)
+				images = append(images, r.visualize(w, h, palette))
+			}
+		}
+	}
+
+	for i, img := range images {
+		f, err := os.OpenFile(fmt.Sprintf("animation/rope%02d.png", i), os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		png.Encode(f, img)
+	}
 }
