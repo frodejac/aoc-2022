70f89ce7f004230e8cbc34cb3351283f7bb31fdd
Frode <frode@remin.no>
2022-12-09 10:53:27 +0100

    Day 8, parts 1 & 2

--- internal/aoc/day08/day08.go
+++ internal/aoc/day08/day08.go
@@ -1,17 +1,168 @@
 package day08
 
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+type direction int
+
+const (
+	up = iota
+	down
+	left
+	right
+)
+
+type move struct {
+	dir direction
+	dst int
+}
+
+type point struct {
+	X int
+	Y int
+}
+type knot struct {
+	position point
+	visited  map[point]int
+}
+
+type rope struct {
+	knots []knot
+}
+
 type Day08 struct {
-	input []byte
+	moves []move
+}
+
+func parseInput(raw []byte) []move {
+	input := string(raw)
+	input = strings.TrimSpace(input)
+	lines := strings.Split(input, "\n")
+	moves := make([]move, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, " ")
+		var dir direction
+		switch parts[0] {
+		case "U":
+			dir = up
+		case "D":
+			dir = down
+		case "L":
+			dir = left
+		case "R":
+			dir = right
+		}
+		distance, _ := strconv.Atoi(parts[1])
+		moves[i] = move{
+			dir: dir,
+			dst: distance,
+		}
+	}
+	return moves
 }
 
 func Solver(input []byte) *Day08 {
-	return &Day08{input: input}
+	return &Day08{
+		moves: parseInput(input),
+	}
+}
+
+func (k *knot) move(d direction) {
+	switch d {
+	case up:
+		k.position.Y++
+	case down:
+		k.position.Y--
+	case left:
+		k.position.X--
+	case right:
+		k.position.X++
+	}
+	k.visited[k.position]++
+}
+
+func (k *knot) update(headX, headY int) {
+	dx := headX - k.position.X
+	dy := headY - k.position.Y
+
+	// Don't move tail if it's already adjacent to head
+	if dx >= -1 && dx <= 1 && dy >= -1 && dy <= 1 {
+		return
+	}
+
+	// Move tail in direction of head
+	if math.Abs(float64(dx)) > math.Abs(float64(dy)) {
+		if dx > 0 {
+			k.position.X++
+		} else {
+			k.position.X--
+		}
+		// Also update row/column if necessary
+		if k.position.Y != headY {
+			if dy > 0 {
+				k.position.Y++
+			} else {
+				k.position.Y--
+			}
+		}
+	} else {
+		if dy > 0 {
+			k.position.Y++
+		} else {
+			k.position.Y--
+		}
+		// Also update row/column if necessary
+		if k.position.X != headX {
+			if dx > 0 {
+				k.position.X++
+			} else {
+				k.position.X--
+			}
+		}
+	}
+	k.visited[k.position]++
+}
+
+func (r *rope) move(d direction, dst int) {
+	for i := 0; i < dst; i++ {
+		r.head().move(d)
+		for j := 1; j < len(r.knots); j++ {
+			r.knots[j].update(r.knots[j-1].position.X, r.knots[j-1].position.Y)
+		}
+	}
+}
+
+func (r *rope) tail() *knot {
+	return &r.knots[len(r.knots)-1]
+}
+
+func (r *rope) head() *knot {
+	return &r.knots[0]
+}
+
+func getRope(len int) rope {
+	r := rope{knots: make([]knot, len)}
+	for i := 0; i < len; i++ {
+		r.knots[i] = knot{position: point{X: 0, Y: 0}, visited: map[point]int{}}
+	}
+	return r
 }
 
 func (d *Day08) SolvePart1() string {
-	return "Day 08, part 1"
+	r := getRope(2)
+	for _, move := range d.moves {
+		r.move(move.dir, move.dst)
+	}
+	return strconv.Itoa(len(r.tail().visited))
 }
 
 func (d *Day08) SolvePart2() string {
-	return "Day 08, part 2"
+	r := getRope(10)
+	for _, move := range d.moves {
+		r.move(move.dir, move.dst)
+	}
+	return strconv.Itoa(len(r.tail().visited))
 }
---
+++ internal/aoc/day08/day08_test.go
@@ -0,0 +1,61 @@
+package day08
+
+import (
+	"os"
+	"path"
+	"runtime"
+	"testing"
+
+	"github.com/frodejac/aoc-2022/internal/io"
+)
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Join(path.Dir(filename), "..", "..", "..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSolveDay08Part1(t *testing.T) {
+	const day, part = 8, 1
+	input, err := io.GetInput(day)
+	if err != nil {
+		t.Logf("Failed to get input for day %02d, part %d", day, part)
+		t.Fail()
+	}
+	solver := Solver(input)
+	part1 := solver.SolvePart1()
+	expected, err := io.GetSolution(day, part)
+	if err != nil {
+		t.Logf("Failed to get solution for day %02d, part %d", day, part)
+		t.Fail()
+		return
+	}
+	if part1 != string(expected) {
+		t.Logf("Expected %s, got %s", expected, part1)
+		t.Fail()
+	}
+}
+
+func TestSolveDay08Part2(t *testing.T) {
+	const day, part = 8, 2
+	input, err := io.GetInput(day)
+	if err != nil {
+		t.Logf("Failed to get input for day %02d, part %d", day, part)
+		t.Fail()
+	}
+	solver := Solver(input)
+	part2 := solver.SolvePart2()
+	expected, err := io.GetSolution(day, part)
+	if err != nil {
+		t.Logf("Failed to get solution for day %02d, part %d", day, part)
+		t.Fail()
+		return
+	}
+	if part2 != string(expected) {
+		t.Logf("Expected %s, got %s", expected, part2)
+		t.Fail()
+	}
+}
