3244de8d56fc4d9d032cbd654d0584b533bc262d
Frode <frode@remin.no>
2022-12-12 13:06:52 +0100

    Dayy 11 visualization

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/frodejac/aoc-2022
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20221126150942-6ab00d035af9 // indirect
+)
--- internal/aoc/day11/day11.go
+++ internal/aoc/day11/day11.go
@@ -1,10 +1,17 @@
 package day11
 
 import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"math"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 type point struct {
@@ -46,7 +53,43 @@ func Solver(input []byte) *Day11 {
 	return &Day11{heightMap: heightMap, startPos: startPos, endPos: endPos}
 }
 
-func dijkstra(w, h int, startPos point, finished func(point) bool, hasPath func(point, point) bool) int16 {
+func rgb(dist int16) color.Color {
+	c1, _ := colorful.Hex("#159CEA")
+	c2, _ := colorful.Hex("#EA6315")
+	ratio := float64(dist) / 380.0
+	return c1.BlendLab(c2, ratio)
+}
+
+func visualize(dist [][]int16, curr point, iteration int, part int) {
+	img := image.NewRGBA(image.Rect(0, 0, len(dist)*3, len(dist[0])*3))
+
+	for i, row := range dist {
+		for j, cell := range row {
+			var c color.Color
+
+			if i == curr.x && j == curr.y {
+				c = color.RGBA{255, 255, 255, 255}
+			} else if cell == math.MaxInt16 {
+				c = color.RGBA{0, 0, 0, 255}
+			} else {
+				c = rgb(cell)
+			}
+			for x := i * 3; x < i*3+3; x++ {
+				for y := j * 3; y < j*3+3; y++ {
+					img.Set(x, y, c)
+				}
+			}
+		}
+	}
+	f, err := os.OpenFile(fmt.Sprintf("animation/path_%d_%06d.png", part, iteration), os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	png.Encode(f, img)
+}
+
+func dijkstra(w, h int, startPos point, finished func(point) bool, hasPath func(point, point) bool, part int) int16 {
 	dist := make([][]int16, w)
 	visited := make([][]bool, w)
 	for i := range dist {
@@ -58,10 +101,12 @@ func dijkstra(w, h int, startPos point, finished func(point) bool, hasPath func(
 	}
 	dist[startPos.x][startPos.y] = 0
 	queue := []point{startPos}
+	c := 0
 	for len(queue) > 0 {
 		curr := queue[0]
 		queue = queue[1:]
-
+		visualize(dist, curr, c, part)
+		c++
 		// Skip visited nodes
 		if visited[curr.x][curr.y] {
 			continue
@@ -114,7 +159,7 @@ func (d *Day11) SolvePart1() string {
 		return (d.heightMap[p2.x][p2.y] - d.heightMap[p1.x][p1.y]) <= 1
 	}
 	w, h := len(d.heightMap), len(d.heightMap[0])
-	cost := dijkstra(w, h, d.startPos, finished, hasPath)
+	cost := dijkstra(w, h, d.startPos, finished, hasPath, 1)
 	return strconv.Itoa(int(cost))
 }
 
@@ -126,6 +171,6 @@ func (d *Day11) SolvePart2() string {
 		return (d.heightMap[p1.x][p1.y] - d.heightMap[p2.x][p2.y]) <= 1
 	}
 	w, h := len(d.heightMap), len(d.heightMap[0])
-	cost := dijkstra(w, h, d.endPos, finished, hasPath)
+	cost := dijkstra(w, h, d.endPos, finished, hasPath, 2)
 	return strconv.Itoa(int(cost))
 }
